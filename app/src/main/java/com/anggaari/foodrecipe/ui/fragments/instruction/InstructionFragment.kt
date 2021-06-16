35fc199227fe630ef63fd0099277b811d5c1b6c8
Angga Ari Wijaya <anggadarkprince@gmail.com>
2021-06-16 09:49:42 +0700

    add instruction design

--- app/src/main/java/com/anggaari/foodrecipe/ui/fragments/instruction/InstructionFragment.kt
+++ app/src/main/java/com/anggaari/foodrecipe/ui/fragments/instruction/InstructionFragment.kt
@@ -5,15 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.anggaari.foodrecipe.R
+import android.webkit.WebViewClient
+import com.anggaari.foodrecipe.databinding.FragmentInstructionBinding
+import com.anggaari.foodrecipe.models.Result
+import com.anggaari.foodrecipe.utils.Constants
 
 class InstructionFragment : Fragment() {
+    private var _binding: FragmentInstructionBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instruction, container, false)
+    ): View {
+        _binding = FragmentInstructionBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(Constants.RECIPE_RESULT_KEY)
+
+        binding.instructionWebView.webViewClient = object : WebViewClient() {}
+        binding.instructionWebView.loadUrl(myBundle!!.sourceUrl)
+
+        return binding.root
     }
 }
